cd599ae28291fae301a1a60e199370791e9e20a2
keisuke10009 <hiruta40@hotmail.com>
2024-11-01 16:35:35 +0900

    学習結果画面実装

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,5 @@
 class DocumentsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index, :new, :show]
   # 問題文一覧画面
   def index
     # 問題文を取得
@@ -11,6 +11,7 @@ class DocumentsController < ApplicationController
   # 学習画面
   def new
     @document = Document.find(params[:id])
+    @vocabularys = @document.vocabulary.split(",")
     @learning_outcome = LearningOutcome.new
     @sum_rel_id = params[:sum_rel_id]
   end
@@ -22,13 +23,16 @@ class DocumentsController < ApplicationController
     response = assess(@document.document, learning_outcome_params[:text], learning_outcome_params[:sum_rel_id])
     @score = score_from_response(response)
     @assessment = assessment_from_resonse(response)
-    binding.pry
     
     @learning_outcome = LearningOutcome.new(learning_outcome_params)
     if @learning_outcome.save
+      # ポイントを10ポイント付与
+      user = current_user
+      new_points = current_user.points + 10
+      current_user.update_column(:points, new_points)
       redirect_to document_path(@learning_outcome.id)
     else
-      render :new, status: :unprocessable_entity
+      redirect_to new_document_path, status: :unprocessable_entity
     end
   end
 
@@ -36,7 +40,10 @@ class DocumentsController < ApplicationController
   def show
     @learning_outcome = LearningOutcome.find(params[:id])
     @document = Document.find(@learning_outcome.document_id)
-    
+    # assessmentから良い点のみを取得
+    @good_points = good_from_assessment(@learning_outcome.assessment)
+    # assessmentから指摘ポイントを取得
+    @improvement_points = improvement_from_assessment(@learning_outcome.assessment)
   end
 
   private
@@ -47,8 +54,8 @@ class DocumentsController < ApplicationController
 
   # プロンプトを作りChatGPTに送信する
   def assess(document, text, sum_rel)
-    prompt = "#{document}の#{sum_rel}は以下になります。#{text}を【点数】XX点、【良い点】3つ、【指摘ポイント】3つの形式で評価してください。"
-           + " 【点数】には0〜100点の範囲で採点し、【良い点】と【指摘ポイント】は、簡潔で具体的にそれぞれ3つずつ挙げてください。"
+    prompt = "#{document}の#{sum_rel}は以下になります。#{text}を【点数】XX点、【良い点】3つ、【改善点】3つの形式で評価してください。"
+           + " 【点数】には0〜100点の範囲で採点し、【良い点】と【改善点】は、簡潔で具体的にそれぞれ3つずつ挙げてください。"
     
     chat_service = ChatGptService.new
     response = chat_service.ask(prompt)
@@ -68,4 +75,30 @@ class DocumentsController < ApplicationController
   def assessment_from_resonse(response)
     response[/(【良い点】.*)/m, 1]
   end
+
+  # アセスメントから良い点を取得
+  def good_from_assessment(assessment)
+    assessments = assessment.split("【改善点】")
+    assessment_array(assessments[0].sub("【良い点】", ""))
+  end
+
+  # アセスメントから改善点を取得
+  def improvement_from_assessment(assessment)
+    assessments = assessment.split("【改善点】")
+    assessment_array(assessments[1].sub("【改善点】", ""))
+  end
+
+  # 良い点や改善点を配列にするメソッド
+  def assessment_array(str)
+    str_array = []
+    str_line = str.sub("\n", "")
+    str_line = str
+    str_1 = str_line.split("2.")[0].sub("1.", "")
+    str_2 = str_line.split("2.")[1].split("3.")[0].sub("2.", "")
+    str_3 = str_line.split("2.")[1].split("3.")[1]
+    str_array.push(str_1.delete(" "))
+    str_array.push(str_2.delete(" "))
+    str_array.push(str_3.delete(" "))
+    str_array
+  end  
 end
--- app/services/chat_gpt_service.rb
+++ app/services/chat_gpt_service.rb
@@ -1,6 +1,8 @@
 require 'faraday_middleware'
 require 'openai'
 
+# ChatGTPにプロンプトを送信しレスポンスを取得する
+# OPENAI_API_KEYは環境変数から取得
 class ChatGptService
   def initialize
     @client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
@@ -9,7 +11,7 @@ class ChatGptService
   def ask(prompt)
     response = @client.chat(
       parameters: {
-        model: "gpt-3.5-turbo", # または "gpt-4"などの利用可能なモデル名
+        model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: prompt }]
       }
     )
